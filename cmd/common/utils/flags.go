openfnord/inspektor-gadget

commit f1cbef78888e09952abae3a28280eb127f8515dd

cmd/deploy: Add container socketpath configuration
When the socketpath of the underlying container runtime is not the
default it can be configured through these newly added cmd options of
the deploy subcmd

cmd/common/utils/flags.go
@@ -20,6 +20,9 @@ import (
 	"strings"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+
+	runtimeclient "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/runtime-client"
 )
 
 const (
@@ -40,6 +43,23 @@ type OutputConfig struct {
 
 	// Verbose prints additional information
 	Verbose bool
+}
+
+func AddOutputFlags(command *cobra.Command, outputConfig *OutputConfig) {
+	command.PersistentFlags().StringVarP(
+		&outputConfig.OutputMode,
+		"output",
+		"o",
+		OutputModeColumns,
+		fmt.Sprintf("Output format (%s).", strings.Join(SupportedOutputModes, ", ")),
+	)
+
+	command.PersistentFlags().BoolVarP(
+		&outputConfig.Verbose,
+		"verbose", "v",
+		false,
+		"Print debug information",
+	)
 }
 
 func (config *OutputConfig) ParseOutputConfig() error {
@@ -74,4 +94,33 @@ func (config *OutputConfig) ParseOutputConfig() error {
 		return WrapInErrInvalidArg("--output / -o",
 			fmt.Errorf("%q is not a valid output format", config.OutputMode))
 	}
+}
+
+type RuntimesSocketPathConfig struct {
+	Docker     string
+	Containerd string
+	Crio       string
+}
+
+func AddRuntimesSocketPathFlags(command *cobra.Command, config *RuntimesSocketPathConfig) {
+	command.PersistentFlags().StringVarP(
+		&config.Docker,
+		"docker-socketpath", "",
+		runtimeclient.DockerDefaultSocketPath,
+		"Docker Engine API Unix socket path",
+	)
+
+	command.PersistentFlags().StringVarP(
+		&config.Containerd,
+		"containerd-socketpath", "",
+		runtimeclient.ContainerdDefaultSocketPath,
+		"containerd CRI Unix socket path",
+	)
+
+	command.PersistentFlags().StringVarP(
+		&config.Crio,
+		"crio-socketpath", "",
+		runtimeclient.CrioDefaultSocketPath,
+		"CRI-O CRI Unix socket path",
+	)
 }

pkg/container-utils/containerutils.go
@@ -24,12 +24,12 @@ import (
 	"strings"
 	"syscall"
 
+	ocispec "github.com/opencontainers/runtime-spec/specs-go"
+
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/containerd"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/crio"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/docker"
 	runtimeclient "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/runtime-client"
-
-	ocispec "github.com/opencontainers/runtime-spec/specs-go"
 )
 
 var AvailableRuntimes = []string{
@@ -46,11 +46,23 @@ type RuntimeConfig struct {
 func NewContainerRuntimeClient(runtime *RuntimeConfig) (runtimeclient.ContainerRuntimeClient, error) {
 	switch runtime.Name {
 	case runtimeclient.DockerName:
-		return docker.NewDockerClient(runtime.SocketPath)
+		socketPath := runtime.SocketPath
+		if envsp := os.Getenv("INSPEKTOR_GADGET_DOCKER_SOCKETPATH"); envsp != "" && socketPath == "" {
+			socketPath = envsp
+		}
+		return docker.NewDockerClient(socketPath)
 	case runtimeclient.ContainerdName:
-		return containerd.NewContainerdClient(runtime.SocketPath)
+		socketPath := runtime.SocketPath
+		if envsp := os.Getenv("INSPEKTOR_GADGET_CONTAINERD_SOCKETPATH"); envsp != "" && socketPath == "" {
+			socketPath = envsp
+		}
+		return containerd.NewContainerdClient(socketPath)
 	case runtimeclient.CrioName:
-		return crio.NewCrioClient(runtime.SocketPath)
+		socketPath := runtime.SocketPath
+		if envsp := os.Getenv("INSPEKTOR_GADGET_CRIO_SOCKETPATH"); envsp != "" && socketPath == "" {
+			socketPath = envsp
+		}
+		return crio.NewCrioClient(socketPath)
 	default:
 		return nil, fmt.Errorf("unknown container runtime: %s (available %s)",
 			runtime, strings.Join(AvailableRuntimes, ", "))

cmd/kubectl-gadget/utils/flags.go
@@ -144,6 +144,8 @@ func AddCommonFlags(command *cobra.Command, params *CommonFlags) {
 	// No 'Namespace' flag because it's added automatically by
 	// KubernetesConfigFlags.AddFlags(rootCmd.PersistentFlags())
 
+	commonutils.AddOutputFlags(command, &params.OutputConfig)
+
 	command.PersistentFlags().StringVarP(
 		&params.LabelsRaw,
 		"selector",
@@ -183,26 +185,9 @@ func AddCommonFlags(command *cobra.Command, params *CommonFlags) {
 		"Show data from pods in all namespaces",
 	)
 
-	command.PersistentFlags().StringVarP(
-		&params.OutputMode,
-		"output",
-		"o",
-		commonutils.OutputModeColumns,
-		fmt.Sprintf("Output format (%s).", strings.Join(commonutils.SupportedOutputModes, ", ")),
-	)
-
-	command.PersistentFlags().BoolVarP(
-		&params.Verbose,
-		"verbose",
-		"",
-		false,
-		"Print additional information",
-	)
-
-	command.PersistentFlags().IntVarP(
+	command.PersistentFlags().IntVar(
 		&params.Timeout,
 		"timeout",
-		"",
 		0,
 		"Number of seconds that the gadget will run for",
 	)

cmd/local-gadget/utils/flags.go
@@ -31,20 +31,14 @@ type CommonFlags struct {
 	// OutputConfig describes the way output should be printed.
 	commonutils.OutputConfig
 
+	// Saves all runtime socket paths
+	commonutils.RuntimesSocketPathConfig
+
 	// Containername allows to filter containers by name.
 	Containername string
 
 	// The name of the container runtimes to be used separated by comma.
 	Runtimes string
-
-	// DockerSocketPath is the Docker Engine API Unix socket path.
-	DockerSocketPath string
-
-	// ContainerdSocketPath is the containerd CRI Unix socket path.
-	ContainerdSocketPath string
-
-	// CrioSocketPath is the CRI-O CRI Unix socket path.
-	CrioSocketPath string
 
 	// RuntimeConfigs contains the list of the container runtimes to be used
 	// with their specific socket path.
@@ -63,11 +57,11 @@ func AddCommonFlags(command *cobra.Command, commonFlags *CommonFlags) {
 
 			switch runtimeName {
 			case runtimeclient.DockerName:
-				socketPath = commonFlags.DockerSocketPath
+				socketPath = commonFlags.RuntimesSocketPathConfig.Docker
 			case runtimeclient.ContainerdName:
-				socketPath = commonFlags.ContainerdSocketPath
+				socketPath = commonFlags.RuntimesSocketPathConfig.Containerd
 			case runtimeclient.CrioName:
-				socketPath = commonFlags.CrioSocketPath
+				socketPath = commonFlags.RuntimesSocketPathConfig.Crio
 			default:
 				return commonutils.WrapInErrInvalidArg("--runtime / -r",
 					fmt.Errorf("runtime %q is not supported", p))
@@ -98,20 +92,8 @@ func AddCommonFlags(command *cobra.Command, commonFlags *CommonFlags) {
 	// do not print usage when there is an error
 	command.SilenceUsage = true
 
-	command.PersistentFlags().BoolVarP(
-		&commonFlags.Verbose,
-		"verbose", "v",
-		false,
-		"Print debug information",
-	)
-
-	command.PersistentFlags().StringVarP(
-		&commonFlags.OutputMode,
-		"output",
-		"o",
-		commonutils.OutputModeColumns,
-		fmt.Sprintf("Output format (%s).", strings.Join(commonutils.SupportedOutputModes, ", ")),
-	)
+	commonutils.AddOutputFlags(command, &commonFlags.OutputConfig)
+	commonutils.AddRuntimesSocketPathFlags(command, &commonFlags.RuntimesSocketPathConfig)
 
 	command.PersistentFlags().StringVarP(
 		&commonFlags.Containername,
@@ -127,26 +109,5 @@ func AddCommonFlags(command *cobra.Command, commonFlags *CommonFlags) {
 		strings.Join(containerutils.AvailableRuntimes, ","),
 		fmt.Sprintf("Container runtimes to be used separated by comma. Supported values are: %s",
 			strings.Join(containerutils.AvailableRuntimes, ", ")),
-	)
-
-	command.PersistentFlags().StringVarP(
-		&commonFlags.DockerSocketPath,
-		"docker-socketpath", "",
-		runtimeclient.DockerDefaultSocketPath,
-		"Docker Engine API Unix socket path",
-	)
-
-	command.PersistentFlags().StringVarP(
-		&commonFlags.ContainerdSocketPath,
-		"containerd-socketpath", "",
-		runtimeclient.ContainerdDefaultSocketPath,
-		"containerd CRI Unix socket path",
-	)
-
-	command.PersistentFlags().StringVarP(
-		&commonFlags.CrioSocketPath,
-		"crio-socketpath", "",
-		runtimeclient.CrioDefaultSocketPath,
-		"CRI-O CRI Unix socket path",
 	)
 }

cmd/kubectl-gadget/deploy.go
@@ -74,9 +74,12 @@ var (
 	quiet               bool
 	debug               bool
 	wait                bool
+	runtimesConfig      commonutils.RuntimesSocketPathConfig
 )
 
 func init() {
+	commonutils.AddRuntimesSocketPathFlags(deployCmd, &runtimesConfig)
+
 	deployCmd.PersistentFlags().StringVarP(
 		&image,
 		"image", "",
@@ -320,6 +323,12 @@ func runDeploy(cmd *cobra.Command, args []string) error {
 					gadgetContainer.Env[i].Value = hookMode
 				case "INSPEKTOR_GADGET_OPTION_FALLBACK_POD_INFORMER":
 					gadgetContainer.Env[i].Value = strconv.FormatBool(fallbackPodInformer)
+				case utils.GadgetEnvironmentContainerdSocketpath:
+					gadgetContainer.Env[i].Value = runtimesConfig.Containerd
+				case utils.GadgetEnvironmentCRIOSocketpath:
+					gadgetContainer.Env[i].Value = runtimesConfig.Crio
+				case utils.GadgetEnvironmentDockerSocketpath:
+					gadgetContainer.Env[i].Value = runtimesConfig.Docker
 				}
 			}
 

cmd/kubectl-gadget/utils/consts.go
@@ -16,4 +16,8 @@ package utils
 
 const (
 	GadgetNamespace string = "gadget"
+
+	GadgetEnvironmentContainerdSocketpath string = "INSPEKTOR_GADGET_CONTAINERD_SOCKETPATH"
+	GadgetEnvironmentCRIOSocketpath       string = "INSPEKTOR_GADGET_CRIO_SOCKETPATH"
+	GadgetEnvironmentDockerSocketpath     string = "INSPEKTOR_GADGET_DOCKER_SOCKETPATH"
 )

pkg/container-utils/docker/docker.go
@@ -24,9 +24,10 @@ import (
 	dockertypes "github.com/docker/docker/api/types"
 	dockerfilters "github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
+	log "github.com/sirupsen/logrus"
+
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/cgroups"
 	runtimeclient "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/runtime-client"
-	log "github.com/sirupsen/logrus"
 )
 
 const (

pkg/container-utils/runtime-client/interface.go
@@ -20,14 +20,15 @@ import (
 )
 
 const (
-	DockerName              = "docker"
-	DockerDefaultSocketPath = "/run/docker.sock"
-
+	// Make sure to keep these settings in sync with pkg/resources/manifests/deploy.yaml
 	CrioName              = "cri-o"
 	CrioDefaultSocketPath = "/run/crio/crio.sock"
 
 	ContainerdName              = "containerd"
 	ContainerdDefaultSocketPath = "/run/containerd/containerd.sock"
+
+	DockerName              = "docker"
+	DockerDefaultSocketPath = "/run/docker.sock"
 )
 
 // ContainerData contains container information returned from the container

commit ade6250381ba3258e59202fbc617d9229c16e0da

treewide: Use pointer for runtime socket.
It fixes a bug where env variable were ignored.

Fixes: f1cbef78888e ("cmd/deploy: Add container socketpath configuration")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

cmd/common/utils/flags.go
@@ -28,6 +28,11 @@ import (
 const (
 	OutputModeJSON          = "json"
 	OutputModeCustomColumns = "custom-columns"
+
+	DockerSocketPathFlag     = "docker-socketpath"
+	ContainerdSocketPathFlag = "containerd-socketpath"
+	CrioSocketPathFlag       = "crio-socketpath"
+	PodmanSocketPathFlag     = "podman-socketpath"
 )
 
 var SupportedOutputModes = []string{OutputModeJSON, OutputModeCustomColumns}
@@ -105,28 +110,28 @@ type RuntimesSocketPathConfig struct {
 func AddRuntimesSocketPathFlags(command *cobra.Command, config *RuntimesSocketPathConfig) {
 	command.PersistentFlags().StringVarP(
 		&config.Docker,
-		"docker-socketpath", "",
+		DockerSocketPathFlag, "",
 		runtimeclient.DockerDefaultSocketPath,
 		"Docker Engine API Unix socket path",
 	)
 
 	command.PersistentFlags().StringVarP(
 		&config.Containerd,
-		"containerd-socketpath", "",
+		ContainerdSocketPathFlag, "",
 		runtimeclient.ContainerdDefaultSocketPath,
 		"containerd CRI Unix socket path",
 	)
 
 	command.PersistentFlags().StringVarP(
 		&config.Crio,
-		"crio-socketpath", "",
+		CrioSocketPathFlag, "",
 		runtimeclient.CrioDefaultSocketPath,
 		"CRI-O CRI Unix socket path",
 	)
 
 	command.PersistentFlags().StringVarP(
 		&config.Podman,
-		"podman-socketpath", "",
+		PodmanSocketPathFlag, "",
 		runtimeclient.PodmanDefaultSocketPath,
 		"Podman Unix socket path",
 	)

cmd/ig/utils/flags.go
@@ -60,17 +60,30 @@ func AddCommonFlags(command *cobra.Command, commonFlags *CommonFlags) {
 	partsLoop:
 		for _, p := range parts {
 			runtimeName := types.String2RuntimeName(strings.TrimSpace(p))
-			socketPath := ""
+			var socketPath *string
+
+			flagSet := cmd.Flags()
+			if flagSet == nil {
+				return fmt.Errorf("nil flag set")
+			}
 
 			switch runtimeName {
 			case types.RuntimeNameDocker:
-				socketPath = commonFlags.RuntimesSocketPathConfig.Docker
+				if flagSet.Changed(commonutils.DockerSocketPathFlag) {
+					socketPath = &commonFlags.RuntimesSocketPathConfig.Docker
+				}
 			case types.RuntimeNameContainerd:
-				socketPath = commonFlags.RuntimesSocketPathConfig.Containerd
+				if flagSet.Changed(commonutils.ContainerdSocketPathFlag) {
+					socketPath = &commonFlags.RuntimesSocketPathConfig.Containerd
+				}
 			case types.RuntimeNameCrio:
-				socketPath = commonFlags.RuntimesSocketPathConfig.Crio
+				if flagSet.Changed(commonutils.CrioSocketPathFlag) {
+					socketPath = &commonFlags.RuntimesSocketPathConfig.Crio
+				}
 			case types.RuntimeNamePodman:
-				socketPath = commonFlags.RuntimesSocketPathConfig.Podman
+				if flagSet.Changed(commonutils.PodmanSocketPathFlag) {
+					socketPath = &commonFlags.RuntimesSocketPathConfig.Podman
+				}
 			default:
 				return commonutils.WrapInErrInvalidArg("--runtime / -r",
 					fmt.Errorf("runtime %q is not supported", p))

pkg/container-utils/containerutils.go
@@ -44,38 +44,61 @@ var AvailableRuntimes = []string{
 
 type RuntimeConfig struct {
 	Name       types.RuntimeName
-	SocketPath string
+	SocketPath *string
 }
 
 func NewContainerRuntimeClient(runtime *RuntimeConfig) (runtimeclient.ContainerRuntimeClient, error) {
 	switch runtime.Name {
 	case types.RuntimeNameDocker:
 		socketPath := runtime.SocketPath
-		if envsp := os.Getenv("INSPEKTOR_GADGET_DOCKER_SOCKETPATH"); envsp != "" && socketPath == "" {
-			socketPath = envsp
+		// If user did not modify the value by using --docker-socketpath, we will
+		// use the env variable if set.
+		// The same applies for other engines.
+		if envsp := os.Getenv("INSPEKTOR_GADGET_DOCKER_SOCKETPATH"); envsp != "" && socketPath == nil {
+			socketPath = &envsp
 		}
-		return docker.NewDockerClient(socketPath)
+
+		if socketPath == nil {
+			return docker.NewDockerClient(runtimeclient.DockerDefaultSocketPath)
+		}
+
+		return docker.NewDockerClient(*socketPath)
 	case types.RuntimeNameContainerd:
 		socketPath := runtime.SocketPath
-		if envsp := os.Getenv("INSPEKTOR_GADGET_CONTAINERD_SOCKETPATH"); envsp != "" && socketPath == "" {
-			socketPath = envsp
+		if envsp := os.Getenv("INSPEKTOR_GADGET_CONTAINERD_SOCKETPATH"); envsp != "" && socketPath == nil {
+			socketPath = &envsp
 		}
-		return containerd.NewContainerdClient(socketPath)
+
+		if socketPath == nil {
+			return containerd.NewContainerdClient(runtimeclient.ContainerdDefaultSocketPath)
+		}
+
+		return containerd.NewContainerdClient(*socketPath)
 	case types.RuntimeNameCrio:
 		socketPath := runtime.SocketPath
-		if envsp := os.Getenv("INSPEKTOR_GADGET_CRIO_SOCKETPATH"); envsp != "" && socketPath == "" {
-			socketPath = envsp
+		if envsp := os.Getenv("INSPEKTOR_GADGET_CRIO_SOCKETPATH"); envsp != "" && socketPath == nil {
+			socketPath = &envsp
 		}
-		return crio.NewCrioClient(socketPath)
+
+		if socketPath == nil {
+			return crio.NewCrioClient(runtimeclient.CrioDefaultSocketPath)
+		}
+
+		return crio.NewCrioClient(*socketPath)
 	case types.RuntimeNamePodman:
 		socketPath := runtime.SocketPath
-		if envsp := os.Getenv("INSPEKTOR_GADGET_PODMAN_SOCKETPATH"); envsp != "" && socketPath == "" {
-			socketPath = envsp
+		if envsp := os.Getenv("INSPEKTOR_GADGET_PODMAN_SOCKETPATH"); envsp != "" && socketPath == nil {
+			socketPath = &envsp
 		}
-		return podman.NewPodmanClient(socketPath), nil
+
+		if socketPath == nil {
+			return podman.NewPodmanClient(runtimeclient.PodmanDefaultSocketPath), nil
+		}
+
+		return podman.NewPodmanClient(*socketPath), nil
 	default:
 		return nil, fmt.Errorf("unknown container runtime: %s (available %s)",
-			runtime, strings.Join(AvailableRuntimes, ", "))
+			runtime.Name, strings.Join(AvailableRuntimes, ", "))
 	}
 }
 

pkg/ig-manager/ig-manager.go
@@ -133,13 +133,13 @@ func isDefaultContainerRuntimeConfig(runtimes []*containerutils.RuntimeConfig) b
 	for _, runtime := range runtimes {
 		switch runtime.Name {
 		case types.RuntimeNameDocker:
-			customSocketPath = runtime.SocketPath != runtimeclient.DockerDefaultSocketPath
+			customSocketPath = *runtime.SocketPath != runtimeclient.DockerDefaultSocketPath
 		case types.RuntimeNameContainerd:
-			customSocketPath = runtime.SocketPath != runtimeclient.ContainerdDefaultSocketPath
+			customSocketPath = *runtime.SocketPath != runtimeclient.ContainerdDefaultSocketPath
 		case types.RuntimeNameCrio:
-			customSocketPath = runtime.SocketPath != runtimeclient.CrioDefaultSocketPath
+			customSocketPath = *runtime.SocketPath != runtimeclient.CrioDefaultSocketPath
 		case types.RuntimeNamePodman:
-			customSocketPath = runtime.SocketPath != runtimeclient.PodmanDefaultSocketPath
+			customSocketPath = *runtime.SocketPath != runtimeclient.PodmanDefaultSocketPath
 		default:
 			customSocketPath = true
 		}

pkg/operators/localmanager/localmanager.go
@@ -185,7 +185,7 @@ partsLoop:
 
 		rc = append(rc, &containerutils.RuntimeConfig{
 			Name:       runtimeName,
-			SocketPath: socketPath,
+			SocketPath: &socketPath,
 		})
 	}
 

pkg/container-utils/containerutils_test.go
@@ -28,7 +28,7 @@ func TestNewContainerRuntimeClient(t *testing.T) {
 	for _, runtime := range AvailableRuntimes {
 		rc := RuntimeConfig{
 			Name:       types.String2RuntimeName(runtime),
-			SocketPath: nonExistingSocketPath,
+			SocketPath: &nonExistingSocketPath,
 		}
 		c, err := NewContainerRuntimeClient(&rc)
 		require.Nil(t, err)